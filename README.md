c908f61a2a73685abfc17bdf7c0752fc397ad9ee
dikkulah <dikkulah@gmail.com>
2022-07-03 17:35:31 +0300

    README.md güncelleme.

--- README.md
+++ README.md
@@ -3,13 +3,13 @@
    1. Mailler mongodb atlasa kaydedildi.
    2. <img src="img/mailsave.png">
    3. repository/jpa klasörü içerisindeki jpa lar ile kayıtlar sağlandı.
-   4. <img src="img/img.png">  tablo yapısı.
+   4. <img src="img/1.png">  tablo yapısı.
 2. JDBC, JdbcTemplate ve Hibernate ile bir DAO katmanını yazın ve avantajlarını ve dezavantajlarını kendi görüşlerinizle
    beraber yazın. OOP’nin polimorfizm’den yararlanarak aynı tabloya üç yöntem ile CRUD işlemlerini yapan kodu yazınız. (
    30 Puan)
-   1. 1
-   2. 2
-   3. 3
+   1.  Till clasının işlemleri TillJpa, TillHibernate, TillJdbcTemplate, Tilljdbc ile yapıldı.
+   2. <img src="img/2.png">
+   3. 
    4. 
 3. Aşağıdaki kavramları örneklerle açıklayın ve hangi problemi nasıl çözdüklerini anlatan bir makale yazın.(Medium’da
    paylaşıp linkini koyabilirsiniz.) (10 Puan)
--- isbasi/src/main/java/org/dikkulah/repository/TillHibernate.java
+++ isbasi/src/main/java/org/dikkulah/repository/TillHibernate.java
@@ -2,29 +2,19 @@ package org.dikkulah.repository;
 
 import org.dikkulah.model.Till;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
-import java.util.Properties;
-
-import static org.hibernate.cfg.AvailableSettings.*;
 
 @Repository
 @Transactional
 public class TillHibernate implements AbstactRepository<Till, Integer> {
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
     @Autowired
     public TillHibernate(EntityManager entityManager) {
         this.entityManager = entityManager;
@@ -38,15 +28,13 @@ public class TillHibernate implements AbstactRepository<Till, Integer> {
 
 
 
-    public Session openCurrentSession() {
+    public void openCurrentSession() {
         currentSession = entityManager.unwrap(Session.class).getSessionFactory().openSession();
-        return currentSession;
     }
 
-    public Session openCurrentSessionwithTransaction() {
-        currentSession = entityManager.unwrap(Session.class).getSessionFactory().openSession();;
+    public void openCurrentSessionwithTransaction() {
+        currentSession = entityManager.unwrap(Session.class).getSessionFactory().openSession();
         currentTransaction = currentSession.beginTransaction();
-        return currentSession;
     }
 
     public void closeCurrentSession() {
